be4fafe45dda8ccf120b92d233d0799f2f15328e
Ziulo <fidelito.olivera@gmail.com>
2025-10-06 14:36:15 -0300

    aja

--- front/profesores/obtener.php
+++ front/profesores/obtener.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="princi.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <link rel="stylesheet" href="princi.css"><title>Document</title>
 </head>
 <body>
     <form id="form-docentes-obtener-ci">
