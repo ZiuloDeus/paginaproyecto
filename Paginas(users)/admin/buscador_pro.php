012c2c6e542af55f57f4f3f06d65b1e814afaf49
dou <camilacontr22@gmail.com>
2025-10-08 12:24:44 -0300

    mmm

--- Paginas(users)/admin/buscador_pro.php
+++ Paginas(users)/admin/buscador_pro.php
@@ -1,16 +1,14 @@
 <?php
 
 require_once "../../registro/conectar.php";
-
 header('Content-Type: application/json');
-
 $conn = conectar_bd();
 
 $busqueda = $_GET['q'] ?? '';
-$sql = "SELECT * FROM profesores WHERE nombre LIKE ?";
+$sql = "SELECT * FROM profesores WHERE nombre LIKE ? OR apellido LIKE ?";
 $stmt = $conn->prepare($sql);
 $like = "%$busqueda%";
-$stmt->bind_param("s", $like);
+$stmt->bind_param("ss", $like, $like);
 $stmt->execute();
 $result = $stmt->get_result();
 
