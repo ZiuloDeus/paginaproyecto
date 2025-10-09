1078cfbea9c98e3df6822a7914375b8af6dd28a5
Ziulo <fidelito.olivera@gmail.com>
2025-10-09 09:46:36 -0300

    Update conexion.php

--- back/agregacion_materia/conexion.php
+++ back/agregacion_materia/conexion.php
@@ -9,25 +9,7 @@ function conectar_bd()
     // Conexión con todos los parámetros
     $conn = mysqli_connect($servidor, $usuario, $pass, $bd);
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $nombre = $_POST['nombre'] ?? '';
-
-    if (!empty($nombre)) {
-        $sql = "INSERT INTO materias (nombre) VALUES (?)";
-        $stmt = mysqli_prepare($conn, $sql);
-        mysqli_stmt_bind_param($stmt, "s", $nombre);
-
-        if (mysqli_stmt_execute($stmt)) {
-            echo "<p style='color: green;'>Materia registrada correctamente.</p>";
-        } else {
-            echo "<p style='color: red;'>Error al registrar: " . mysqli_error($conn) . "</p>";
-        }
-
-        mysqli_stmt_close($stmt);
-    } else {
-        echo "<p style='color: red;'>Por favor, ingresa un nombre.</p>";
-    }
-}
+    
 
     if (!$conn) {
         die("Error de conexion " . mysqli_connect_error());
