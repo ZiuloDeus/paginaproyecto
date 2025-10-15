86744e416beba65a5b8d45a81e921ccbce5e5b72
dou <camilacontr22@gmail.com>
2025-10-15 09:32:43 -0300

    qeqe

--- front/inicio/profe/prof_inicio.js
+++ front/inicio/profe/prof_inicio.js
@@ -56,9 +56,9 @@ loginForm.addEventListener('submit', function(e) {
     return;
   }
   showMessage('Inicio de sesión exitoso!' , 'success');
-  window.location.href = '../Paginas(users)/profe/panel.html' // redirigir a pagina principal de admin
+  window.location.href = '../profe/panel.html' // redirigir a pagina principal de admin
 });
 
 document.getElementById('irRegistro').addEventListener('click', function() {
-  window.location.href = '../registro/registro.html'; // redirigir a pagina de registro
+  window.location.href = 'C:\Users\Admin\Documents\GitHub\paginaproyecto\front\aver_profes';
 });
